5d17aaf8cde6e04b21bb8b04b97ec23680127b26
Md. Fazle Rabbi Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-08-17 12:17:01 +0600

    Add Navbar and Footer components, implement Logo and NavLink, and style headings

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -14,7 +16,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.className} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header className="py-2 md:w-11/12 mx-auto">
+          <Navbar></Navbar>
+        </header>
+
+        <main className="py-2 md:w-11/12 mx-auto">{children}</main>
+
+        <footer>
+          <Footer></Footer>
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="text-2xl flex items-center gap-1">
+      <Image src="/assets/logo.png" alt="logo" width={50} height={50} />
+      <h2>
+        Hero <span className="text-primary">Kidz</span>
+      </h2>
+    </Link>
+  );
+}
