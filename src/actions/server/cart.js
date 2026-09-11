99c10d3d611d46dc58584ff9c87c36af8721cef3
Md. Fazle Rabbi Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-09-11 06:59:34 +0600

    feat: implement cart handling logic to add/update items based on user session

--- src/actions/server/cart.js
+++ src/actions/server/cart.js
@@ -7,9 +7,43 @@ import { getServerSession } from "next-auth";
 const cartCollections = connect("cart");
 
 export const handleCart = async ({ product, inc = true }) => {
-  const user = await getServerSession(authOptions);
+  const { user } = (await getServerSession(authOptions)) || {};
 
-  console.log(user);
+  if (!user) {
+    return { success: false };
+  }
 
-  return { success: true };
+  // Find the existing cart item for the current user and product
+  const cartItemQuery = { email: user?.email, productId: product?._id };
+  const existingCartItem = await cartCollections.findOne(cartItemQuery);
+
+  if (existingCartItem) {
+    // Update the quantity of the existing cart item
+    const quantityUpdate = {
+      $inc: {
+        quantity: inc ? 1 : -1,
+      },
+    };
+
+    const result = await cartCollections.updateOne(
+      cartItemQuery,
+      quantityUpdate,
+    );
+
+    return { success: Boolean(result.modifiedCount) };
+  } else {
+    // Add the product as a new cart item
+    const newCartItem = {
+      productId: product?._id,
+      username: user?.name,
+      email: user?.email,
+      title: product?.title,
+      quantity: 1,
+      image: product?.image,
+      price: product?.price - (product?.price * product?.discount) / 100,
+    };
+    const result = await cartCollections.insertOne(newCartItem);
+
+    return { success: result.acknowledged };
+  }
 };
